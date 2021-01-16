ce3230dca6074993b921ea7069160acb20860e63
maks <a@a.a>
2021-01-16 11:04:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+
+const Login =({login, registration})=>{
+
+  const [loginValue, setLoginValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const changeLogin = (e)=>{
+    setLoginValue(e.target.value);
+
+  }
+
+  const changePassword = (e)=>{
+    setPasswordValue(e.target.value);
+  } 
+  // https://learn.javascript.ru/promise
+
+  return(
+    <div className="login">
+      <input type="text" placeholder="login"  value={loginValue} onChange={changeLogin}></input>
+      <input type="password" placeholder="password" value={passwordValue} onChange={changePassword}></input>
+
+    {/* здесь будет вызов функции, которая будет ждать и давать ответ через секунду
+    если grisha grisha - то ок,
+    если нет - то еррор */}
+      <button onClick={(e)=>{console.log(`login is ${loginValue}, password ${passwordValue}`) } }>{login}</button>
+      <button>{registration}</button>
+    </div>
+  )
+  
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login login="Log in" registration="Reg"/>
   );
 }
 
