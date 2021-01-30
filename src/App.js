d0d5ca3a432c6a8a20d6b22137a715e074c5b5f2
maks <a@a.a>
2021-01-30 11:03:53 +0300

    promise

--- src/App.js
+++ src/App.js
@@ -8,16 +8,57 @@ const Login =({login, registration})=>{
 
   const [loginValue, setLoginValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
+  const [disabledValue, setDisabledvalue] = useState(true);
 
   const changeLogin = (e)=>{
     setLoginValue(e.target.value);
+    if(e.target.value.length && passwordValue.length){
+      setDisabledvalue(false)
+    }
+    else{
+      setDisabledvalue(true)
+    }
 
   }
 
   const changePassword = (e)=>{
     setPasswordValue(e.target.value);
+    if(loginValue.length && e.target.value.length){
+      setDisabledvalue(false)
+    }
+    else{
+      setDisabledvalue(true)
+    }
   } 
   // https://learn.javascript.ru/promise
+
+
+
+
+  const doLogin = (login, password) =>{
+    return new Promise(  (resolve, reject)=>{
+
+      setTimeout( ()=>{
+        if( ('grisha' === login) && (password === 'grisha') ){
+          resolve(1)
+        }
+        else{
+          reject("error")
+        }
+    
+      } ,2000 )
+
+    }  ) 
+  }
+
+  // когда промис разрешился (удачно или с ошибкой), надо разблокировать кнопку
+  // если неудачно, то вывести на экран
+  // для этого создать элемент (спан) со стэйтом внутри
+  // при ошибке стейт меняется - задается текст ошибки
+  // если всё сделаете, выводить ошибки логин неправильный или пароль неправильный
+  // для этого разделить условие проверки на две проверки
+
+
 
   return(  
     <div>
@@ -25,31 +66,13 @@ const Login =({login, registration})=>{
         <input type="text" placeholder="login"  value={loginValue} onChange={changeLogin}></input>
         <input type="password" placeholder="password" value={passwordValue} onChange={changePassword}></input>
  
-
-     
-        let promise = new Promise((resolve, reject)   {
-
-          setTimeout('promise', 2000)
-          });
-
-          promise
-          .then(
-            result  {
-              alert("все ок")
-            },
-            error  {
-              alert("ошибка")
-            }
-          );
-          resolve();
-                              
-                
-
       {/* здесь будет вызов функции, которая будет ждать и давать ответ через секунду
       если grisha grisha - то ок,
       если нет - то еррор */}
              
-        <button onClick={(e)=>{console.log(`login is ${loginValue}, password ${passwordValue}`) } }>{login}</button>
+
+             {/* сюда нужен стейт, дизэйблд */}
+        <button disabled={disabledValue} onClick={(e)=>{ setDisabledvalue(true); doLogin(loginValue, passwordValue).then((result)=>console.log(result)).catch(e => console.log(e))} }>{login}</button>
         <button>{registration}</button>
       </div>
     </div>
